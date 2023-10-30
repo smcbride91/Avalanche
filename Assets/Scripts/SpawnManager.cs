88d25b4d280f255874265fa02cbd640eef402a01
smcbride91 <146111365+smcbride91@users.noreply.github.com>
2023-10-30 16:27:10 +0000

    Obstacle spawning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,11 +7,11 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] obstaclePrefabs;
     private float spawnRangeXLeft = -22.2f;
     private float spawnRangeXRight = 18.83f;
-;
-;
+
     private float spawnPosZ = 26.33f;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
+    private float spawnPosY = 9.77f;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,7 +28,7 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomObstacle()
     {
         int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
-        Vector3 spawnPos = new Vector3(Random.Range(spawnRangeXLeft, spawnRangeXRight), 0, spawnPosZ);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnRangeXLeft, spawnRangeXRight), spawnPosY, spawnPosZ);
 
         Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
 
