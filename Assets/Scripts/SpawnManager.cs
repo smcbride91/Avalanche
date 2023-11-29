7027f5b160d408c2f10d7ab8173c6bce45767736
smcbride91 <146111365+smcbride91@users.noreply.github.com>
2023-11-29 18:18:15 +0000

    Increase Speed

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -14,10 +14,13 @@ public class SpawnManager : MonoBehaviour
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
     private float spawnPosY = 9.77f;
+    private float increaseSpeedDelay;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnRandomObstacle", startDelay, spawnInterval);
+        increaseSpeedDelay= startDelay * 2;
+        InvokeRepeating ("IncreaseSpeed",increaseSpeedDelay,increaseSpeedDelay);
         score = 0;
         scoreText.text = "Score: " + score;
 
@@ -38,6 +41,11 @@ public class SpawnManager : MonoBehaviour
         Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
         UpdateScore(5);
 
+    }
+
+    void IncreaseSpeed()
+    {
+        spawnInterval = spawnInterval * 2;
     }
 
 
