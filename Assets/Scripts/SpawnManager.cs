e0214b69036bd0a97b7427afe72029e9a75a8bfa
smcbride91 <146111365+smcbride91@users.noreply.github.com>
2024-01-05 12:52:59 +0000

    Avalanche fog

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
+
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] obstaclePrefabs;
@@ -10,6 +12,7 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeXLeft = -22.2f;
     private float spawnRangeXRight = 18.83f;
     private int score;
+    private int scoreIncrease = 5;
     private float spawnPosZ = 26.33f;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
@@ -22,7 +25,8 @@ public class SpawnManager : MonoBehaviour
         increaseSpeedDelay= startDelay * 2;
         InvokeRepeating ("IncreaseSpeed",increaseSpeedDelay,increaseSpeedDelay);
         score = 0;
-        scoreText.text = "Score: " + score;
+        //scoreText.text = "Score: " + Convert.ToString(score);
+        //scoreText.text = "Score: " + score;
 
     }
 
@@ -35,24 +39,25 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnRandomObstacle()
     {
-        int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
-        Vector3 spawnPos = new Vector3(Random.Range(spawnRangeXLeft, spawnRangeXRight), spawnPosY, spawnPosZ);
+        int obstacleIndex = UnityEngine.Random.Range(0, obstaclePrefabs.Length);
+        Vector3 spawnPos = new Vector3(UnityEngine.Random.Range(spawnRangeXLeft, spawnRangeXRight), spawnPosY, spawnPosZ);
 
-        Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
-        UpdateScore(5);
+        //Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
+        UpdateScore(scoreIncrease);
 
     }
 
     void IncreaseSpeed()
     {
         spawnInterval = spawnInterval * 2;
+        Console.WriteLine("SpawnInterval " + Convert.ToString(spawnInterval));
     }
 
 
     void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "Score: " + score;
+        //scoreText.text = "Score: " + score;
 
     }
 }
